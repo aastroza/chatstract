e265e453e51ddcbd9bc5cee9f289eb974275415d
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2024-05-11 16:05:07 -0400

    new readme

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 # chatstract
 
 **Chatstract** is a tool that progressively extracts structured information from multi-turn user chats using pydantic schemas and LLMs.
+
+## Basic Example
+
+[Code](examples/simple_model.py)
+
+First, create the pydantic model you wish to fill out through a conversational method.
+
+```python
+from datetime import date
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Task(BaseModel):
+    title: Optional[str] = Field(description="A short title of the task.", default=None)
+    due_date: Optional[date] = Field(description="The deadline for the task. Current year: 2024", default=None)
+    responsable: Optional[str] = Field(description="The person assigned to solve the task", default=None)
+    location: Optional[str] = Field(description="The location where the task will be performed", default=None)
+```
+
+Then, use Chatstract to populate the data model with values from sequential messages provided by the user.
+
+```python
+from chatstract.core import Chat
+
+chat = Chat()
+
+messages = ["Jack has to clean his desk",
+            "The deadline is september 7th",
+            "This task is for the Downtown office",
+            "I messed up, the task is for Dorothy at the Uptown office",]
+
+for message in messages:
+    info = chat.chat_ai(message = message, data_model = Task)
+    print(f"User message: {message}")
+    print(f"Data: {chat.data}")
+    print(f"Missing information: {info.missing_information}")
+    print(f"Follow-up question: {info.question}\n")
+```
+
+Below is the output you can expect:
+
+```shell
+User message: Jack has to clean his desk
+Data: {'title': 'Clean the desk', 'due_date': None, 'responsable': 'Jack', 'location': None}
+Missing information: ['due_date', 'location']
+Follow-up question: Could you please provide the due date and location for the task 'Clean the desk'?
+
+User message: The deadline is september 7th
+Data: {'title': 'Clean the desk', 'due_date': datetime.date(2024, 9, 7), 'responsable': 'Jack', 'location': None}
+Missing information: ['location']
+Follow-up question: Could you please provide the location where the task 'Clean the desk' should be performed?
+
+User message: This task is for the Downtown office
+Data: {'title': 'Clean the desk', 'due_date': datetime.date(2024, 9, 7), 'responsable': 'Jack', 'location': 'Downtown office'}
+Missing information: []
+Follow-up question: None
+
+User message: I messed up, the task is for Dorothy at the Uptown office
+Data: {'title': 'Clean the desk', 'due_date': datetime.date(2024, 9, 7), 'responsable': 'Dorothy', 'location': 'Uptown office'}
+Missing information: []
+Follow-up question: None
+```
--- src/chatstract/core.py
+++ src/chatstract/core.py
@@ -4,8 +4,10 @@ import instructor
 from openai import OpenAI
 
 class Conversation(BaseModel):
-    missing_information: List[str] = Field(description="List of missing information about the data.")
-    question: Optional[str] = Field(description="A follow up question to ask the user about the missing information", default=None)
+    missing_information: List[str] = Field(description="List of keys from the dictionary where the associated values are None, indicating missing information.",
+                                           default = [])
+    question: Optional[str] = Field(description="A follow-up question to ask the user, aimed at gathering the missing information identified by the keys.",
+                                    default=None)
 
 class Chat(BaseModel):
     """Handles chat interactions with an AI model to discuss missing data."""
@@ -22,20 +24,27 @@ class Chat(BaseModel):
                 temperature = 0,
                 response_model=data_model,
                 messages=[{"role": "user", "content": message}],
-            )
+            ).model_dump()
         else:
-            self.data = client.chat.completions.create(
+            new_data = client.chat.completions.create(
                 model="gpt-4",
                 temperature = 0,
                 response_model=data_model,
                 messages=[
                     {
+
                         "role": "system",
-                        "content": f"{message}",
+                        "content": """You are an iterative data completion assistant.
+                                    You will receive a Python dictionary containing some existing data values and a user message that includes updates or new data.
+                                    Please carefully analyze the user's message, extract relevant information, and update the dictionary accordingly.
+                                    Ensure that the updated dictionary is accurate and reflects all new data provided by the user.""",
                     },
-                    {"role": "user", "content": message},
+                    {"role": "user", "content": f"current dict: {self.data}"},
+                    {"role": "user", "content": f"message: {message}"},
                 ],
-            )
+            ).model_dump()
+
+            self.data = {key: (new_data[key] if new_data[key] is not None else self.data[key]) for key in self.data}
 
         return client.chat.completions.create(
             model="gpt-4",
@@ -44,8 +53,10 @@ class Chat(BaseModel):
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a world class conversational algorithm to ask questions about missing data values. You are going to receive a python dict with some missing data values. Please ask the user for the missing information.",
+                    "content": """You are a world class conversational algorithm to ask questions about missing data values.
+                                You are going to receive a python dict with some missing data values.
+                                Please ask the user for the missing information.""",
                 },
-                {"role": "user", "content": f"{dict(self.data)}"},
+                {"role": "user", "content": f"{self.data}"},
             ]
         )
